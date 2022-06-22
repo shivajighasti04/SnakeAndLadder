a4a2db7e771df180fca2b7f61e329b2e9da36df2
shivajighasti04 <shivajighasti97@gamil.com>
2022-06-23 00:28:15 +0530

    Snake and Ladder game played with single player at start position 0

---
+++ SnakeAndLadderUc1.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.snakeandladders;
+
+/**
+ * Program for Snake and Ladder game played with single player at start position 0.
+ * 
+ * @author Shivaji Ghasti
+ *
+ */
+public class SnakeAndLadderUc1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Snake And ladder Program");
+		System.out.println("Lets play");
+
+		// Start position
+		int position = 0;
+	}
+}
+
+
